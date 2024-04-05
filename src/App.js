c9d484350d5f9f02f6f0c89fdcbab52e8e95e2c7
Nhlakanipho Khoza <nhlakanipho.khoza@durban.gov.za>
2024-04-05 09:04:14 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import Homepage from './Homepage';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="app">
+      <Homepage/>  
     </div>
   );
 }
