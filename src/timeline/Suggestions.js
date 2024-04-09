205ccbd2e5006aba70df3640f73344c8b740d9b5
Nhlakanipho Khoza <nhlakanipho.khoza@durban.gov.za>
2024-04-09 16:29:27 +0200

    adding suggestions

--- src/timeline/Suggestions.js
+++ src/timeline/Suggestions.js
@@ -1,9 +1,67 @@
 import React from 'react';
 import "./Suggestions.css";
+import { Avatar } from '@mui/material';
 
 function Suggestions() {
   return (
-    <div>Suggestions</div>
+    <div className='Sugessions'>
+        <div className='sugessions__title'>Sugessions for you</div>
+        <div className='sugessions__usernames'>
+
+
+            <div className='sugessions__username'>
+                <div className='username__left'>
+                    <span className='avatar'>
+                        <Avatar>R</Avatar>
+                    </span>
+                    <div className='userName__info'>
+                        <span className='username'>Romeo_ </span>
+                        <span className='relations'>New to Instagram</span>
+                    </div>
+                </div>
+                <button className='follow__button'>Follow</button>
+            </div>
+
+            <div className='sugessions__username'>
+                <div className='username__left'>
+                    <span className='avatar'>
+                        <Avatar>R</Avatar>
+                    </span>
+                    <div className='userName__info'>
+                        <span className='username'>Romeo_</span>
+                        <span className='relations'>New to Instagram</span>
+                    </div>
+                </div>
+                <button className='follow__button'>Follow</button>
+            </div>
+
+            <div className='sugessions__username'>
+                <div className='username__left'>
+                    <span className='avatar'>
+                        <Avatar>R</Avatar>
+                    </span>
+                    <div className='userName__info'>
+                        <span className='username'>Romeo_</span>
+                        <span className='relations'>New to Instagram</span>
+                    </div>
+                </div>
+                <button className='follow__button'>Follow</button>
+            </div>
+
+            <div className='sugessions__username'>
+                <div className='username__left'>
+                    <span className='avatar'>
+                        <Avatar>R</Avatar>
+                    </span>
+                    <div className='userName__info'>
+                        <span className='username'>Romeo_</span>
+                        <span className='relations'>New to Instagram</span>
+                    </div>
+                </div>
+                <button className='follow__button'>Follow</button>
+            </div>
+        </div>
+    </div>
   )
 }
 
--- src/timeline/Timeline.js
+++ src/timeline/Timeline.js
@@ -1,15 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Timeline.css";  
 import Suggestions from './Suggestions';
 import Post from './posts/Post';
 
 function Timeline() {
+    const [posts, setPosts] = useState([
+        {
+            user: "Kante",
+            postImage: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9hwqSa-83Oxmq_1gPXvGS5L3XgZbbM3YqWRpeu2Y_QGWKBzAuMBarxfBV9LLwksS7ELg&usqp=CAU",
+            likes: 652,
+            timestamp: "1d"
+        },
+        {
+            user: "Messi ",
+            postImage: "https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/bltb6caf4f8bb6dface/60dc10a7993b043846449946/5f27af6bee8c2ec7981a77519a0ae258bd346068.jpg?auto=webp&format=pjpg&width=3840&quality=60",
+            likes: 977,
+            timestamp: "11h"
+        },
+        {
+            user: "Zidane ",
+            postImage: "https://fifpro.org/media/fhmfhvkx/messi-world-cup.jpg",
+            likes: 977,
+            timestamp: "3h"
+        },
+        {
+            user: "Hazard",
+            postImage: "https://notjustok.com/wp-content/uploads/2023/10/20231010_111825-scaled.jpg",
+            likes: 588,
+            timestamp: "28min"
+        },
+    ]);
   return (
     <div className="timeline">
         
     <div className="timeline__left">
     <div className="timeline__posts">
-        <Post />
+        {posts.map(post=> (
+            <Post user = {post.user} 
+             postImage={post.postImage} 
+             likes={post.likes} 
+            timestamp={post.timestamp} />
+
+        ))}
     </div>
         </div> 
     <div className="timeline__right">
